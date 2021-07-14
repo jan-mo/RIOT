bce595a7ae331c1defefac0d2fda46c8c1ee1d71
Jan Mohr <jan.mohr@ml-pa.com>
2021-07-14 22:15:08 +0200

    compression: changed zlib and heatshrink compression; added compressed and decompressed files

--- compareFirmware/compression/LZSS_heatshrink.py
+++ compareFirmware/compression/LZSS_heatshrink.py
@@ -3,38 +3,38 @@
 import heatshrink2 as hs
 import os
 
-
-folder = 'bin_files/'
-
-
-# compress all bin-files in folder bin_files
-files = os.listdir(folder)
-
-# compress
-for file in files:
-	fin = open(folder + file,'rb').read()
-
-	compress = hs.compress(fin)
-
-	file_name = file[:-4]
-	name_compress = file_name + '_heat_compressed'
-
-	fout = open('compressed/' + name_compress, 'wb')
-	fout.write(compress)
-	fout.close()
+board_folder = ['../database/samd20-xpro/bin_files/', '../database/samd21-xpro/bin_files/']
 
 
-# decompress
-for file in files:
-	
-	file_name = file[:-4]
-	name_compress = file_name + '_heat_compressed'
-	name_decompress = file_name + '_heat_decompressed.bin'
+for folder in board_folder:
+	# compress all bin-files in folder bin_files
+	files = os.listdir(folder)
 
-	fout = open('compressed/' + name_compress, 'rb').read()
+	# compress
+	for file in files:
+		fin = open(folder + file,'rb').read()
 
-	decompress = hs.decompress(fout)
+		compress = hs.compress(fin)
 
-	fout = open('decompressed/' + name_decompress, 'wb')
-	fout.write(decompress)
-	fout.close()
+		file_name = file[:-4]
+		name_compress = file_name + '_heat_compressed'
+
+		fout = open('compressed/' + name_compress, 'wb')
+		fout.write(compress)
+		fout.close()
+
+
+	# decompress
+	for file in files:
+		
+		file_name = file[:-4]
+		name_compress = file_name + '_heat_compressed'
+		name_decompress = file_name + '_heat_decompressed.bin'
+
+		fout = open('compressed/' + name_compress, 'rb').read()
+
+		decompress = hs.decompress(fout)
+
+		fout = open('decompressed/' + name_decompress, 'wb')
+		fout.write(decompress)
+		fout.close()
--- compareFirmware/compression/zlib_compress.py
+++ compareFirmware/compression/zlib_compress.py
@@ -3,35 +3,36 @@
 import zlib
 import os
 
-folder = 'bin_files/'
+board_folder = ['../database/samd20-xpro/bin_files/', '../database/samd21-xpro/bin_files/']
 
 
-# compress all bin-files in folder bin_files
-files = os.listdir(folder)
+for folder in board_folder:
+	# compress all bin-files in folder bin_files
+	files = os.listdir(folder)
 
-# compress
-for file in files:
-	f = open(folder + file, 'rb').read()
-	compress = zlib.compress(f, zlib.Z_BEST_COMPRESSION)
+	# compress
+	for file in files:
+		f = open(folder + file, 'rb').read()
+		compress = zlib.compress(f, zlib.Z_BEST_COMPRESSION)
 
-	file_name = file[:-4]
-	name_compress = file_name + '_zlib_compressed'
+		file_name = file[:-4]
+		name_compress = file_name + '_zlib_compressed'
 
-	f = open('compressed/' + name_compress, 'wb')
-	f.write(compress)
-	f.close()
+		f = open('compressed/' + name_compress, 'wb')
+		f.write(compress)
+		f.close()
 
 
-# decompress
-for file in files:
+	# decompress
+	for file in files:
 
-	file_name = file[:-4]
-	name_compress = file_name + '_zlib_compressed'
-	name_decompress = file_name + '_zlib_decompressed.bin'
+		file_name = file[:-4]
+		name_compress = file_name + '_zlib_compressed'
+		name_decompress = file_name + '_zlib_decompressed.bin'
 
-	f = open('compressed/' + name_compress, 'rb').read()
-	decompress = zlib.decompress(f)
+		f = open('compressed/' + name_compress, 'rb').read()
+		decompress = zlib.decompress(f)
 
-	f = open('decompressed/' + name_decompress, 'wb')
-	f.write(decompress)
-	f.close()
+		f = open('decompressed/' + name_decompress, 'wb')
+		f.write(decompress)
+		f.close()
