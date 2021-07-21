2c7c8b2a01aca197584ccfd5405c7e578b20aa81
Jan Mohr <jan.mohr@campus.tu-berlin.de>
2021-07-21 19:50:56 +0200

    difference/deltagen_diff: added script

---
+++ compareFirmware/difference/deltagen_diff/deltagen_diff.bat
@@ -0,0 +1,3 @@
+
+DeltaGenerator.exe -p "..\..\database\%1\samd20-xpro\%1.bin" -n "..\..\database\%2\samd20-xpro\%2.bin" --log > "samd20-xpro\%1__%2.log" -o "samd20-xpro\%1__%2.bin"
+DeltaGenerator.exe -p "..\..\database\%1\samd21-xpro\%1.bin" -n "..\..\database\%2\samd21-xpro\%2.bin" --log > "samd21-xpro\%1__%2.log" -o "samd21-xpro\%1__%2.bin"
