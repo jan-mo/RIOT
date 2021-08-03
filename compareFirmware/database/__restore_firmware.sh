d02fabd11ce001e5c45386f9f2f2c5b49c258c80
Jan Mohr <jan.mohr@campus.tu-berlin.de>
2021-08-03 09:44:02 +0200

    database: stash check in restore_firmware script

--- compareFirmware/database/__restore_firmware.sh
+++ compareFirmware/database/__restore_firmware.sh
@@ -1,5 +1,14 @@
 # save current version in git stash
-git stash
+echo "Stash current git? [y/N]"
+read answer
+
+if [ -z "$answer" ] || [ $answer != y ]
+then
+    echo No stash.
+else
+	git stash
+	echo Git was stashed!
+fi
 
 cd ../..
 
