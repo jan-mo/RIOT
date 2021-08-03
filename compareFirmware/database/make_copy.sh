6452e5a09ceb542b0be2827e35033e7d39ab4f42
Jan Mohr <jan.mohr@campus.tu-berlin.de>
2021-08-03 15:57:16 +0200

    database: modified diff command in make_copy script

--- compareFirmware/database/make_copy.sh
+++ compareFirmware/database/make_copy.sh
@@ -1,6 +1,6 @@
 cd ../firmware_example/
 sudo make -j BOARD=samd20-xpro
 sudo make -j BOARD=samd21-xpro
-git diff > firmware.diff
+git diff thesis/rev_00 ../firmware_example/ > firmware.diff
 cd -
 ./__copy_bin_elf.py
