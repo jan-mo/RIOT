9223f4471b451b46680ff8c85bbb9fc9c6d2af24
Jan Mohr <jan.mohr@campus.tu-berlin.de>
2021-08-17 15:47:43 +0200

    difference: changes in difference.py

--- compareFirmware/difference/difference.py
+++ compareFirmware/difference/difference.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3.9
 
-import os, sys, json
+import os, sys, json, distro
 from shutil import move, copyfile
-from joblib import Parallel, delayed
 import matplotlib.pyplot as plt
 import numpy as np
-from pathos.multiprocessing import ThreadingPool as TPool
+# parallel processing
 from pathos.multiprocessing import ProcessingPool as Pool
+from pathos.helpers import mp as helper
 
 ###
 ### calculates differences in firmware-database
@@ -14,22 +14,30 @@ from pathos.multiprocessing import ProcessingPool as Pool
 
 # used differencing algos
 diff_algos = ["diff", "bsdiff", "xdelta3", "rsync8", "rsync16", "rsync32"] # bdelta - not implemented right
+pkg_arch = ["diffutils bsdiff xdelta3 rsync"]
+pkg_ubuntu = ["diffutils bsdiff xdelta3 rsync"]
 
+# database
 database = os.listdir('../database')
 versions = []
 files_samd20 = []
 files_samd21 = []
 
-# create path for samd20 and samd21
+### searching path for samd20 and samd21 ###
 for version in database:
     if os.path.isdir(os.path.join('../database/' + version)):
         # exclude suit_updater
         if version == "suit_updater":
             continue;
 
+        # collection all versions
         versions.append(version)
         files_samd20.append('../database/' + version + '/samd20-xpro/' + version + '.bin')
         files_samd21.append('../database/' + version + '/samd21-xpro/' + version + '.bin')
+
+        # remove old files
+        os.system("rm -rf ../database/" + version + '/samd20-xpro/' + version + '.bin_*')
+        os.system("rm -rf ../database/" + version + '/samd21-xpro/' + version + '.bin_*')
 
 files_samd20 = sorted(files_samd20)
 files_samd21 = sorted(files_samd21)
@@ -37,8 +45,17 @@ files_samd21 = sorted(files_samd21)
 folder = "algo_diffs/"
 folder_patch = "algo_diffs/patched/"
 
-# calculate all differences
+### clear algo_diffs folder ###
+os.system("sudo rm -r " + folder)
+os.system("mkdir " + folder)
+os.system("mkdir " + folder_patch)
+
+### calculate all differences ###
 def second_loop(i, file1, files, sizes, name_arch):
+    # getting PID
+    pid = helper.current_process().pid
+    print(name_arch + " differences of Rev_" + str(i) + " PID:" + str(pid))
+
     for j, file2 in enumerate(files):
 
         #### diff ####
@@ -84,78 +101,75 @@ def second_loop(i, file1, files, sizes, name_arch):
         if "rsync8" in diff_algos:
             name_rsync8 = "rsync8_rev" + str(i) + "_rev" + str(j)
             folder_rsync8 = folder + "rsync8/" + name_arch + "/" + name_rsync8
-            rsync8_par = "_rsync8_" + str(i) + str(j)     # save file for parallel loop
+            rsync8_par = "_rsync8_" + str(pid)     # save file for parallel loop
             # diff file
             os.system("cp " + file1 + " " + file1 + rsync8_par)
-            os.system("cp " + file2 + " " + file2 + rsync8_par)
+            if i != j :
+                os.system("cp " + file2 + " " + file2 + rsync8_par)
             os.system("rsync -arvq -B=8 --only-write-batch=" + folder_rsync8 + " " + file2 + rsync8_par + " " + file1 + rsync8_par)
             # patch file
             os.system("rsync -arvq -B=8 --read-batch=" + folder_rsync8 + " " + file1 + rsync8_par)
-            os.system("cp " + file1 + rsync8_par + " " + folder_patch + name_rsync8)
+            os.system("mv " + file1 + rsync8_par + " " + folder_patch + name_rsync8)
+            if i != j:
+                os.system("rm " + file2 + rsync8_par)
 
         #### rsync16 ####
         if "rsync16" in diff_algos:
             name_rsync16 = "rsync16_rev" + str(i) + "_rev" + str(j)
             folder_rsync16 = folder + "rsync16/" + name_arch + "/" + name_rsync16
-            rsync16_par = "_rsync16_" + str(i) + str(j)     # save file for parallel loop
+            rsync16_par = "_rsync16_" + str(pid)     # save file for parallel loop
             # diff file
             os.system("cp " + file1 + " " + file1 + rsync16_par)
-            os.system("cp " + file2 + " " + file2 + rsync16_par)
+            if i != j:
+                os.system("cp " + file2 + " " + file2 + rsync16_par)
             os.system("rsync -arvq -B=16 --only-write-batch=" + folder_rsync16 + " " + file2 + rsync16_par + " " + file1 + rsync16_par)
             # patch file
             os.system("rsync -arvq -B=16 --read-batch=" + folder_rsync16 + " " + file1 + rsync16_par)
-            os.system("cp " + file1 + rsync16_par + " " + folder_patch + name_rsync16)
+            os.system("mv " + file1 + rsync16_par + " " + folder_patch + name_rsync16)
+            if i != j:
+                os.system("rm " + file2 + rsync16_par)
 
         #### rsync32 ####
         if "rsync32" in diff_algos:
             name_rsync32 = "rsync32_rev" + str(i) + "_rev" + str(j)
             folder_rsync32 = folder + "rsync32/" + name_arch + "/" + name_rsync32
-            rsync32_par = "_rsync32_" + str(i) + str(j)     # save file for parallel loop
+            rsync32_par = "_rsync32_" + str(pid)     # save file for parallel loop
             # diff file
             os.system("cp " + file1 + " " + file1 + rsync32_par)
-            os.system("cp " + file2 + " " + file2 + rsync32_par)
+            if i != j:
+                os.system("cp " + file2 + " " + file2 + rsync32_par)
             os.system("rsync -arvq -B=32 --only-write-batch=" + folder_rsync32 + " " + file2 + rsync32_par + " " + file1 + rsync32_par)
             # patch file
             os.system("rsync -arvq -B=32 --read-batch=" + folder_rsync32 + " " + file1 + rsync32_par)
-            os.system("cp " + file1 + rsync32_par + " " + folder_patch + name_rsync32)
-
-
+            os.system("mv " + file1 + rsync32_par + " " + folder_patch + name_rsync32)
+            if i != j:
+                os.system("rm " + file2 + rsync32_par)
 
         #### check patch and calc sizes ####
         if "diff" in diff_algos:
             sizes["diff"][name_diff] = {"size":os.path.getsize(folder_diff),
                                         "check":"pass" if os.path.getsize(file2) == os.path.getsize(folder_patch + name_diff) else "fail"}
-        
         if "bsdiff" in diff_algos:
             sizes["bsdiff"][name_bsdiff] = {"size":os.path.getsize(folder_bsdiff),
                                             "check":"pass" if os.path.getsize(file2) == os.path.getsize(folder_patch + name_bsdiff) else "fail"}
-
         if "xdelta3" in diff_algos:
             sizes["xdelta3"][name_xdelta3] = {"size":os.path.getsize(folder_xdelta3),
                                               "check":"pass" if os.path.getsize(file2) == os.path.getsize(folder_patch + name_xdelta3) else "fail"}
-
         if "bdelta" in diff_algos:
             sizes["bdelta"][name_bdelta] = os.path.getsize(folder + name_bdelta)
-
         if "rsync8" in diff_algos:
             sizes["rsync8"][name_rsync8] = {"size":os.path.getsize(folder_rsync8),
                                             "check":"pass" if os.path.getsize(file2) == os.path.getsize(folder_patch + name_rsync8) else "fail"}
-
         if "rsync16" in diff_algos:        
             sizes["rsync16"][name_rsync16] = {"size":os.path.getsize(folder_rsync16),
                                               "check":"pass" if os.path.getsize(file2) == os.path.getsize(folder_patch + name_rsync16) else "fail"}
-
         if "rsync32" in diff_algos:        
             sizes["rsync32"][name_rsync32] = {"size":os.path.getsize(folder_rsync32),
                                               "check":"pass" if os.path.getsize(file2) == os.path.getsize(folder_patch + name_rsync32) else "fail"}
-
     return [sizes]
 
-# clear folder differences
-os.system("rm -r " + folder + "*")
 
-# create algo folders
-os.system("mkdir " + folder_patch)
+### create algo folders ###
 for algo in diff_algos:
     os.system("mkdir " + folder + algo)
     os.system("mkdir " + folder + algo + "/samd20-xpro/")
@@ -168,10 +182,15 @@ for algo in diff_algos:
     sizes_samd20[algo] = dict()
     sizes_samd21[algo] = dict()
 
+### checking difftools installed ###
+### supporting Ubuntu (apt-get) and Arch (yay)
+dist = distro.linux_distribution()
+if dist == "Arch Linux":
+    os.system("yay -S " + pkg_arch)
+elif dist == "Ubuntu":
+    os.system("sudo apt-get install " + pkg_arch)
+
 ### samd20-xpro ###
-#results_samd20 = [Parallel(n_jobs=-1)(delayed(second_loop)(i, file1, files_samd20, sizes_samd20, "samd20-xpro") for i, file1 in enumerate(files_samd20))]
-
-
 num = []
 file1 = []
 files = []
@@ -184,17 +203,13 @@ for i, file in enumerate(files_samd20):
     sizes.append(sizes_samd20)
     arch.append("samd20-xpro")
 
-result = TPool().amap(second_loop, num, file1, files, sizes, arch)
-
+result = Pool().amap(second_loop, num, file1, files, sizes, arch)
 results_samd20 = result.get()
 
 # clear patched folder
 os.system("rm -r " + folder_patch + "*")
 
 ### samd21-xpro ###
-#results_samd21 = Parallel(n_jobs=-1)(delayed(second_loop)(i, file1, files_samd21, sizes_samd21, "samd21-xpro") for i, file1 in enumerate(files_samd21))
-
-
 num = []
 file1 = []
 files = []
@@ -207,16 +222,8 @@ for i, file in enumerate(files_samd21):
     sizes.append(sizes_samd21)
     arch.append("samd21-xpro")
 
-result = TPool().amap(second_loop, num, file1, files, sizes, arch)
-
+result = Pool().amap(second_loop, num, file1, files, sizes, arch)
 results_samd21 = result.get()
-
-
-# clear database
-for file in files_samd20:
-    os.system("rm " + file + "_*")
-for file in files_samd21:
-    os.system("rm " + file + "_*")
 
 ### clear data ###
 os.system("rm -r " + folder_patch)
@@ -235,7 +242,7 @@ for algo in diff_algos:
     sizes_all_arch["samd20-xpro"][algo] = dict()
     sizes_all_arch["samd21-xpro"][algo] = dict()
 
-# add dict samd20
+### add samd20 to dict ###
 for result in results_samd20:
     for elem in result:
         for algo in diff_algos:
@@ -243,14 +250,14 @@ for result in results_samd20:
                 sizes_all_arch["samd20-xpro"][algo][key] = elem[algo][key]
             
 
-# add dict samd21
+### add samd21 to dict ###
 for result in results_samd21:
     for elem in result:
         for algo in diff_algos:
             for key in elem[algo].keys():
                 sizes_all_arch["samd21-xpro"][algo][key] = elem[algo][key]
 
-# sorting dict
+### sorting dict ###
 sizes_sorted = dict()
 sizes_sorted["samd20-xpro"] = dict()
 sizes_sorted["samd21-xpro"] = dict()
@@ -277,7 +284,7 @@ def plot_bar(values, xlabels, legend, name_fig, ylabel="size [Byte]"):
     length = len(legend)
 
     for i, value in enumerate(values):
-        offset = x + ((i-length/2) * width) if i < length/2 else x + ((i-length/2+1) * width)
+        offset = x + ((i-length/2) * width + width/2) if i < length/2 else x + ((i-length/2+1) * width - width/2)
         ax.bar(offset, value, width, label=legend[i])
 
     # Add some text for labels, title and custom x-axis tick labels, etc.
@@ -310,8 +317,6 @@ for algo in diff_algos:
 
 fig_samd20 = plot_bar(array_all, labels, diff_algos, "SAMD20-xpro differencing algorithms")
 
-
-
 ### SAMD21 bar plot ###
 labels = []
 revs = sizes_sorted["samd21-xpro"][diff_algos[0]].keys()
@@ -329,8 +334,6 @@ for algo in diff_algos:
     array_all.append(array)
 
 fig_samd21 = plot_bar(array_all, labels, diff_algos, "SAMD21-xpro differencing algorithms")
-
-#plt.show()
 
 ### save plots to file ###
 fig_samd20.savefig("diffalgos_samd20.pdf")
