19b54c706b9893031026960742e5d01349edcb3a
Jan Mohr <jan.mohr@campus.tu-berlin.de>
2021-08-18 13:32:43 +0200

    database: saving firmware.diff as splitted files

--- compareFirmware/database/__copy_bin_elf.py
+++ compareFirmware/database/__copy_bin_elf.py
@@ -19,27 +19,31 @@ name = sys.argv[1]
 
 # check if firmware already exists
 try:
-	os.mkdir(name)
+    os.mkdir(name)
 except FileExistsError:
-	print("ERROR: Firmware version exists!")
-	sys.exit()
+    print("ERROR: Firmware version exists!")
+    sys.exit()
 
 
 for board in boards:
-	# create board folder
-	os.mkdir(name + '/' + board)
+    # create board folder
+    os.mkdir(name + '/' + board)
 
-	# copy bin files
-	copyfile(src + board + '/firmware_example.bin', name + '/' + board + '/' + name + '.bin')
+    # copy bin files
+    copyfile(src + board + '/firmware_example.bin', name + '/' + board + '/' + name + '.bin')
 
-	#copy elf files
-	copyfile(src + board + '/firmware_example.elf', name + '/' + board + '/' + name + '.elf')
+    #copy elf files
+    copyfile(src + board + '/firmware_example.elf', name + '/' + board + '/' + name + '.elf')
 
-	#copy map files
-	copyfile(src + board + '/firmware_example.map', name + '/' + board + '/' + name + '.map')
+    #copy map files
+    copyfile(src + board + '/firmware_example.map', name + '/' + board + '/' + name + '.map')
 
 
 # save diff of firmware version
 move(src + '../firmware.diff', name + '/firmware.diff')
+
+if os.path.getsize(name + "/firmware.diff") >= 6000000:
+    os.system("split -b50M " + name + "/firmware.diff " + name + "/firmware.diff_split_")
+    os.system("rm " + name + "/firmware.diff")
 
 print('Done!')
--- compareFirmware/database/make_copy.sh
+++ compareFirmware/database/make_copy.sh
@@ -7,8 +7,8 @@ then
     echo "Stash is necessary!"
     exit
 else
-	git stash
-	echo "Git was stashed!"
+    git stash
+    echo "Git was stashed!"
 fi
 
 echo "Enter firmware revision:"
@@ -22,12 +22,6 @@ sudo make -j BOARD=samd21-xpro
 
 git diff thesis/rev_00 > firmware.diff
 size = $(wc -c < firmware.diff)
-
-if [ $size >= 6000000 ]
-then
-	split -b50M firmware.diff firmware.diff_split_
-	rm firmware.diff
-fi
 
 git checkout thesis/checking_firmware_versions
 
