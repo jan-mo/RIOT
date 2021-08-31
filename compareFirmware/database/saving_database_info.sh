da0c8e911449e0d4c52f5664b8d256bea862f42f
Jan Mohr <jan.mohr@ml-pa.com>
2021-08-31 17:18:36 +0200

    database: adding new revisions to database
    - updating rev_07, rev_08, rev_09, rev_10

--- compareFirmware/database/saving_database_info.sh
+++ compareFirmware/database/saving_database_info.sh
@@ -4,7 +4,7 @@ git tag -n thesis/rev* > revision_tags.txt
 
 # saving size of revision bin-files
 echo "SAMD20 bin sizes [Byte]:" > revision_bin_sizes.txt
-du -ab --max-depth=3 | sort -h | grep bin | grep rev | grep samd20 >> revision_bin_sizes.txt
+du -ab --max-depth=3 | sort -k 2| grep bin | grep rev | grep samd20 >> revision_bin_sizes.txt
 echo "" >> revision_bin_sizes.txt
 echo "SAMD21 bin sizes [Byte]:" >> revision_bin_sizes.txt
-du -ab --max-depth=3 | sort -h | grep bin | grep rev | grep samd21 >> revision_bin_sizes.txt
+du -ab --max-depth=3 | sort -k 2 | grep bin | grep rev | grep samd21 >> revision_bin_sizes.txt
