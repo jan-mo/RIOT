758166f66c710bc6d139886e963faed9a145502a
Jan Mohr <jan.mohr@ml-pa.com>
2021-09-14 20:10:51 +0200

    database: adding slots for all revisions

---
+++ compareFirmware/database/create_slots.sh
@@ -0,0 +1,43 @@
+# save current version in git stash
+git status
+echo ""
+echo "Stash current git (necessary)? [y/N]"
+read answer
+
+if [ -z "$answer" ] || [ $answer != y ]
+then
+    echo "Stash is necessary!"
+    exit
+else
+    git stash
+    echo "Git was stashed!"
+fi
+
+echo "Enter firmware revision:"
+read version
+
+git checkout thesis/${version}
+cd ../../firmwareExample/
+sudo rm -r bin/
+
+# update revision to add riotboot
+./riotboot_patch.sh
+
+sudo make -j BOARD=samd20-xpro
+sudo make -j BOARD=samd21-xpro
+
+# remove changes
+git stash
+git checkout thesis/checking_firmware_versions
+
+# copy slots to revision folder
+cd ../compareFirmware/database
+mkdir riotboot/${version}
+mkdir riotboot/${version}/samd20-xpro
+mkdir riotboot/${version}/samd21-xpro
+cp ../../firmwareExample/bin/samd20-xpro/firmware_example-slot0.bin ./riotboot/${version}/samd20-xpro/${version}_slot0.bin
+cp ../../firmwareExample/bin/samd20-xpro/firmware_example-slot1.bin ./riotboot/${version}/samd20-xpro/${version}_slot1.bin
+cp ../../firmwareExample/bin/samd21-xpro/firmware_example-slot0.bin ./riotboot/${version}/samd21-xpro/${version}_slot0.bin
+cp ../../firmwareExample/bin/samd21-xpro/firmware_example-slot1.bin ./riotboot/${version}/samd21-xpro/${version}_slot1.bin
+
+echo Done!
