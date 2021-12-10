1a6b4e3b56b04c5505dae3aba8931f7359cd3a02
Jan Mohr <jan.mohr@campus.tu-berlin.de>
2021-12-10 17:49:19 +0100

    differnces: added file to compare minibs_heat offset to bsdiff

---
+++ compareFirmware/difference/scripts/test_size_minibs_bsdiff.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3.9
+
+import json
+
+with open("../output/sizes_sorted.save", 'r') as json_file:
+    sizes_sorted = json.load(json_file)
+with open("../output/versions.save", 'r') as file:
+    versions = json.load(file)
+
+minibs_keys = []
+bsdiff_keys = []
+
+for i, elem in enumerate(versions[:-1]):
+    rev1 = elem
+    rev2 = versions[i+1]
+    minibs_keys.append("minibs_heat_" + rev1 + "_" + rev2)
+    bsdiff_keys.append("bsdiff_" + rev1 + "_" + rev2)
+
+minibs_sizes = []
+bsdiff_sizes = []
+
+for elem in minibs_keys:
+    minibs_sizes.append(sizes_sorted["samd21-xpro"]["minibs_heat"][elem]["size"])
+
+for elem in bsdiff_keys:
+    bsdiff_sizes.append(sizes_sorted["samd21-xpro"]["bsdiff"][elem]["size"])
+
+print(minibs_sizes)
+print(bsdiff_sizes)
+
+for i, elem in enumerate(minibs_sizes):
+    print(elem - bsdiff_sizes[i])
