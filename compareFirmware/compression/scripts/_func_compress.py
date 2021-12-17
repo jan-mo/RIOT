75a0e02cd34445f54b7953a85ae46372d6190a15
Jan Mohr <jan.mohr@ml-pa.com>
2021-12-17 17:46:43 +0100

    python: removed python3.* set to python 3

--- compareFirmware/compression/scripts/_func_compress.py
+++ compareFirmware/compression/scripts/_func_compress.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.9
+#!/usr/bin/env python3
 
 import zlib, gzip, bz2, lzma
 import heatshrink2 as hs
--- compareFirmware/difference/scripts/difference_riotboot.py
+++ compareFirmware/difference/scripts/difference_riotboot.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.9
+#!/usr/bin/env python3
 
 import os, json
 from __calc_diffs import calcDiff
